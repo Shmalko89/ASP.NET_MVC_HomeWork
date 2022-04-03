18981a220e4caba2e9ab110890ec7e8c35f3e720
Vladimir <shmalko89@list.ru>
2022-04-03 18:27:41 +0400

    add homework3

---
+++ HW3/SimpleCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace HW3;
+
+internal class SimpleCommand :ICommand
+{
+    public ViewBaseModel ViewBaseModel { get; set; }
+    public SimpleCommand(ViewBaseModel viewBaseModel)
+    {
+        this.ViewBaseModel = viewBaseModel;
+    }
+    public event EventHandler CanExecuteChanged;
+
+    public bool CanExecute(object parameter)
+    {
+        return true;
+    }
+
+    public void Execute(object parameter)
+    {
+        this.ViewBaseModel.SimpleMethod();
+    }
+}
