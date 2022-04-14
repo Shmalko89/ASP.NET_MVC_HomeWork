25274c39c736844923ee129d06839372b48f2d65
Vladimir <shmalko89@list.ru>
2022-04-14 19:41:52 +0400

    add logic

---
+++ Scaner/ScanerLibrary/Data/Data.cs
@@ -0,0 +1,10 @@
+﻿using Scaner.ScanerLibrary.Interfaces;
+
+namespace Scaner.ScanerLibrary.Data;
+
+public struct Data : IData
+{
+    public int CpuMetric { get; }
+
+    public int MemoryMetric { get; }
+}
---
+++ Scaner/ScanerLibrary/Scaner/ScanerDevice.cs
@@ -0,0 +1,27 @@
+﻿using Scaner.ScanerLibrary.Interfaces;
+
+namespace Scaner.ScanerLibrary.Scaner;
+
+internal class ScanerDevice : IScanerDevice
+{
+    private string _path;
+
+    public ScanerDevice(string path)
+    {
+        _path = path;
+    }
+    public Stream Scan()
+    {
+        var memorystream = new MemoryStream();
+        var stream = new StreamReader(_path);
+        var line = stream.ReadLine();
+        while (line != null)
+        {
+            Console.WriteLine(line);
+            line = stream.ReadLine();
+            Console.WriteLine();
+        }
+        return memorystream;
+    }
+
+}
