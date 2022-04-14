60ee8a01e5626f60d3c9218b25e67a1ace55ff90
Vladimir <shmalko89@list.ru>
2022-04-14 20:25:44 +0400

    рефакторинг

---
+++ Scaner/ScanerLibrary/Logger/Logger.cs
@@ -0,0 +1,11 @@
+﻿using Scaner.ScanerLibrary.Interfaces;
+
+namespace Scaner.ScanerLibrary;
+
+internal class Logger : ILogger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine(message); 
+    }
+}
